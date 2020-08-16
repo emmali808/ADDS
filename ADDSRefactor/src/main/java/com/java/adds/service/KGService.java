8729ca8f59db5070a70661ddad614b3072896644
Dylan <xieyixiang5.20@hotmail.com>
2020-08-17 02:06:38 +0800

    update import graph

--- ADDSRefactor/src/main/java/com/java/adds/service/KGService.java
+++ ADDSRefactor/src/main/java/com/java/adds/service/KGService.java
@@ -10,6 +10,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -55,28 +56,44 @@ public class KGService {
         BufferedReader reader = null;
         try {
             reader = new BufferedReader(new FileReader((file)));
+            String idPatternString = "t (\\d+)";
+            String vPatternString = "v (\\d+) (.*)";
+            String aPatternString = "a (\\w+) (.*)";
+            String ePatternString = "e (\\d+) (\\d+)";
+
+            Pattern idPattern = Pattern.compile(idPatternString);
+            Pattern vPattern = Pattern.compile(vPatternString);
+            Pattern aPattern = Pattern.compile(aPatternString);
+            Pattern ePattern = Pattern.compile(ePatternString);
+
             String tempString = null;
-            while((tempString = reader.readLine()) != null) {
-                String idPatternString = "t #(\\d+)";
-                String vPatternString = "v (\\d+) (.*)";
-                String ePatternString = "e (\\d+) (\\d+)";
-
-                Pattern idPattern = Pattern.compile(idPatternString);
-                Pattern vPattern = Pattern.compile(vPatternString);
-                Pattern ePattern = Pattern.compile(ePatternString);
-
+            tempString = reader.readLine();
+            while(tempString != null) {
+                Long graphId = (long) 0;
                 Matcher idMatcher = idPattern.matcher(tempString);
                 Matcher vMatcher = vPattern.matcher(tempString);
                 Matcher eMatcher = ePattern.matcher(tempString);
-
-                Long graphId = (long) 0;
                 if (idMatcher.find()) {
                     graphId = Long.parseLong(idMatcher.group(1));
                 } else if (vMatcher.find()) {
-                    kgDao.createNode((long)41, "41_" + vMatcher.group(1), vMatcher.group(2) );
+                    Map<String, String> attributes = new LinkedHashMap<>();
+                    tempString = reader.readLine();
+                    while(tempString != null) {
+                        Matcher aMatcher = aPattern.matcher(tempString);
+                        if (aMatcher.find()) {
+                            attributes.put(aMatcher.group(1), aMatcher.group(2));
+                            tempString = reader.readLine();
+                        }
+                        else {
+                            kgDao.createNode((long)41, vMatcher.group(1), vMatcher.group(2), attributes);
+                            break;
+                        }
+                    }
+                    continue;
                 } else if (eMatcher.find()) {
-                    kgDao.createRel((long)41, "41_" + eMatcher.group(1), "41_" + eMatcher.group(2) );
+                    kgDao.createRel((long)41, eMatcher.group(1), eMatcher.group(2) );
                 }
+                tempString = reader.readLine();
             }
             reader.close();
         } catch (IOException e) {
