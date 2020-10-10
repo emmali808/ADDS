bf6e4aa8a30208c9a32f85ed961e45c5021f69b2
Dylan <xieyixiang5.20@hotmail.com>
2020-10-10 22:32:09 +0800

    medical archive preprocess update static page

--- ADDSRefactor/frontend/src/pages/medicalArchivePreprocess.vue
+++ ADDSRefactor/frontend/src/pages/medicalArchivePreprocess.vue
@@ -3,7 +3,54 @@
     <el-container>
       <NavigationBar/>
       <el-container class="content-div">
-        
+
+        <el-header>
+          <el-button type="primary" @click="openUploadDialog">Upload Medical Archive</el-button>
+        </el-header>
+
+        <el-main>
+          <el-table
+              :data="tableData" height="500" style="width: 100%;">
+            <el-table-column prop="title" label="Title" align="center"></el-table-column>
+            <el-table-column prop="category" label="Category" align="center"></el-table-column>
+            <el-table-column label="Status" align="center">
+              <template slot-scope="scope">
+                <el-tag
+                    :type="scope.row.finished ? 'success' : 'danger'"
+                    disable-transitions>{{scope.row.finished ? 'Processed' : 'Processing'}}
+                </el-tag>
+              </template>
+            </el-table-column>
+            <el-table-column label="Options" align="center">
+              <template slot-scope="scope">
+                <el-button type="primary" size="small" :disabled="!scope.row.finished">Download</el-button>
+              </template>
+            </el-table-column>
+          </el-table>
+        </el-main>
+
+        <el-dialog
+            title="Upload Medical Archive"
+            :visible.sync="uploadFormVisible"
+            width="640px"
+            append-to-body
+            :close-on-click-modal="false"
+            :close-on-press-escape="false"
+            :show-close="false">
+          <el-form ref="uploadForm" :model="uploadForm" :rules="rules" label-position="left" label-width="130px">
+            <el-form-item label="Name" prop="name">
+              <el-input v-model="uploadForm.name"></el-input>
+            </el-form-item>
+            <el-form-item label="Description" prop="desc">
+              <el-input type="textarea" :rows="5" v-model="uploadForm.desc"></el-input>
+            </el-form-item>
+          </el-form>
+          <div slot="footer">
+            <el-button type="primary" @click="upload">Submit</el-button>
+            <el-button @click="cancelUpload">Cancel</el-button>
+          </div>
+        </el-dialog>
+
       </el-container>
     </el-container>
   </div>
@@ -15,16 +62,95 @@
         name: "medicalArchivePreprocess",
         components: {
             NavigationBar
+        },
+        methods:{
+          openUploadDialog() {
+            this.uploadFormVisible = true;
+          },
+          upload() {
+            this.uploadFormVisible = false;
+          },
+          cancelUpload() {
+            this.uploadFormVisible = false;
+          }
+        },
+        data: function() {
+            return {
+                uploadFormVisible: false,
+                uploadForm: {
+                    name: '',
+                    // category: '',
+                    desc: '',
+                },
+                rules: {
+                    name: [
+                        {required: true, message: 'Please input name', trigger: 'blur'}
+                        // {min: 1, max: 10, message: 'Name length is between 1 and 10', trigger: 'blur'}
+                    ],
+                    desc: [
+                        {required: true, message: 'Please input description', trigger: 'blur'}
+                    ]
+                },
+                tableData:[
+                    {
+                        title:"Orthopaedic dataset1",
+                        category:"Orthopaedic",
+                        finished:true
+                    },
+                    {
+                        title:"Orthopaedic dataset2",
+                        category:"Orthopaedic",
+                        finished:true
+                    },
+                    {
+                        title:"Orthopaedic dataset3",
+                        category:"Orthopaedic",
+                        finished:false
+                    },
+                    {
+                        title:"Hepatobiliary surgery dataset1",
+                        category:"Hepatobiliary surgery",
+                        finished:true
+                    },
+                    {
+                        title:"Hepatobiliary surgery dataset2",
+                        category:"Hepatobiliary surgery",
+                        finished:false
+                    },
+                    {
+                        title:"Hepatobiliary surgery dataset3",
+                        category:"Hepatobiliary surgery",
+                        finished:false
+                    },
+                    {
+                        title:"Hepatobiliary surgery dataset4",
+                        category:"Hepatobiliary surgery",
+                        finished:false
+                    },
+                    {
+                        title:"Hepatobiliary surgery dataset5",
+                        category:"Hepatobiliary surgery",
+                        finished:false
+                    },
+                    {
+                        title:"Hepatobiliary surgery dataset6",
+                        category:"Hepatobiliary surgery",
+                        finished:false
+                    }
+
+                ]
+            }
         }
     }
 </script>
 
 <style scoped>
   .content-div {
-    position: fixed;
-    top: 0;
-    left: 30px;
-    right: 30px;
-    bottom: 0;
+    position: relative;
+    top: 110px;
+  }
+
+  #paginator {
+    text-align: center;
   }
 </style>
