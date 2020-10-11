8e0873a3a87bf599ae8d8e15719fbd1e9181480c
Dylan <xieyixiang5.20@hotmail.com>
2020-10-12 00:31:33 +0800

    medical archive preprocess finish frontend

--- ADDSRefactor/frontend/src/pages/medicalArchivePreprocess.vue
+++ ADDSRefactor/frontend/src/pages/medicalArchivePreprocess.vue
@@ -41,6 +41,28 @@
             <el-form-item label="Name" prop="name">
               <el-input v-model="uploadForm.name"></el-input>
             </el-form-item>
+            <el-form-item label="Category" prop="category">
+              <el-select v-model="uploadForm.category" placeholder="Please select category">
+                <el-option label="Orthopaedic" value="Orthopaedic"></el-option>
+                <el-option label="Hepatobiliary surgery" value="Hepatobiliary surgery"></el-option>
+                <el-option label="Cardiovascular Surgery" value="Cardiovascular Surgery"></el-option>
+                <el-option label="Endocrinology" value="Endocrinology"></el-option>
+                <el-option label="Cardiology" value="Cardiology"></el-option>
+              </el-select>
+            </el-form-item>
+            <el-form-item label="Upload File" prop="uploadFile">
+              <el-upload
+                ref="upload"
+                action=""
+                accept=".zip"
+                :multiple="false"
+                :file-list="fileList"
+                :show-file-list="true"
+                :http-request="uploadMedicalArchive"
+                :auto-upload="false">
+                <el-button slot="trigger" type="primary">Browse</el-button>
+              </el-upload>
+            </el-form-item>
             <el-form-item label="Description" prop="desc">
               <el-input type="textarea" :rows="5" v-model="uploadForm.desc"></el-input>
             </el-form-item>
@@ -64,28 +86,76 @@
             NavigationBar
         },
         methods:{
+          loadArchiveList() {
+            this.$axios({
+              method: 'get',
+              url: '/archive/' + this.$store.state.user.id,
+            }).then(res => {
+              this.tableData.length = 0;
+              for (let row in res.data) {
+                if (res.data.hasOwnProperty(row)) {
+                  this.tableData.push({
+                    value: res.data[row].id,
+                    label: res.data[row].name
+                  });
+                }
+              }
+            }).catch(error => {
+              console.log(error);
+            });
+          },
           openUploadDialog() {
             this.uploadFormVisible = true;
           },
           upload() {
-            this.uploadFormVisible = false;
+            this.$refs.upload.submit();
           },
           cancelUpload() {
+            this.$refs['uploadForm'].resetFields();
             this.uploadFormVisible = false;
+          },
+          uploadMedicalArchive(content) {
+            let params = new FormData();
+            params.append("name", this.uploadForm.name);
+            params.append("category", this.uploadForm.category);
+            params.append("desc", this.uploadForm.desc);
+            params.append("file", content.file);
+
+            this.$axios({
+              method: 'post',
+              url: '/archive/upload/' + this.$store.state.user.id,
+              data: params
+            }).then(res => {
+              this.$refs['uploadForm'].resetFields();
+              this.uploadFormVisible = false;
+            }).catch(error => {
+              this.$message({
+                type: 'error',
+                message: 'Failed to upload! Check console.',
+                showClose: true
+              });
+              console.log(error);
+            });
           }
         },
         data: function() {
             return {
                 uploadFormVisible: false,
+                fileList: [],
                 uploadForm: {
                     name: '',
-                    // category: '',
+                    category: 'Orthopaedic',
                     desc: '',
                 },
                 rules: {
                     name: [
                         {required: true, message: 'Please input name', trigger: 'blur'}
-                        // {min: 1, max: 10, message: 'Name length is between 1 and 10', trigger: 'blur'}
+                    ],
+                    category: [
+                        {required: true, message: 'Please select category', trigger: 'blur'}
+                    ],
+                    uploadFile: [
+                        {required: true, message: 'Please select file', trigger: 'change'}
                     ],
                     desc: [
                         {required: true, message: 'Please input description', trigger: 'blur'}
@@ -140,7 +210,10 @@
 
                 ]
             }
-        }
+        },
+        // created() {
+        //     this.loadArchiveList();
+        // },
     }
 </script>
 
